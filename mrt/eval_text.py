2682ddb666523687a10f207292518e593ff94d69
Chris Kedzie <kedzie@cs.columbia.edu>
2020-05-16 11:38:12 -0400

    Updated eval and generate tasks.

--- mrt/eval_text.py
+++ mrt/eval_text.py
@@ -2,7 +2,7 @@ import argparse
 from pathlib import Path
 import json
 import numpy as np
-from collections import Counter
+from collections import Counter, defaultdict
 from subprocess import check_output, DEVNULL
 from tempfile import NamedTemporaryFile
 from pprint import pformat
@@ -38,6 +38,40 @@ def prf(ref, predicted):
         f1 = 0.
     return prec, recall, f1
 
+def sem_err(ref, predicted):
+    ref = list(ref)
+    added = []
+    incorrect = []
+    missing = []
+    ref_slot2val = defaultdict(list)
+    for sv in ref:
+        s, v = sv.split("=")
+        ref_slot2val[s].append(v)
+
+    for sv in predicted:
+        s,v = sv.split("=")
+        if s not in ref_slot2val:
+            added.append(sv)
+        else:
+            if v not in ref_slot2val[s]:
+                incorrect.append(sv)
+                #if s not in ['genres', 'platforms', 'player_perspectice']:
+                #    if len(ref_slot2val[s]) > 0:
+                #        ref_slot2val[s].pop(0)
+
+            #else:
+            #    ref_slot2val[s].pop(ref_slot2val[s].index(v))
+    missing = [sf for sf in ref if sf not in predicted]
+    #for s, vals in ref_slot2val.items():
+    ##    for v in vals:
+     #       missing.append(f'{s}={v}')
+
+    total = len(added) + len(incorrect) + len(missing)
+    counts = [len(missing), len(incorrect), len(added), total]
+
+    return {"missing": missing, "incorrect": incorrect, 
+            "added": added, "counts": counts}
+
 def bigrams(x):
     return list(zip(['@'] + x, x + ['@']))
 
@@ -65,7 +99,8 @@ def rerank_beam(outputs, ref_linear_mr, mr_utils, rules):
         
     return outputs[np.argmax(beam_scores)]
 
-def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
+def evaluate(script_path, refs, in_fp, rule_set, eval_lin_strat, name, 
+             eval_log_dir):
     if rule_set == 'Viggo':
         mr_utils = mrt.viggo.mr_utils
         rules = mrt.viggo.rules
@@ -102,8 +137,8 @@ def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
 
         pred = json.loads(pred_line)
 
-        canon_ref_lmr = mr_utils.remove_header(
-            mr_utils.linearize_mr(ref['source']['mr'], delex=delex == 'delex'))
+        canon_ref_lmr = list(set(mr_utils.remove_header(
+            ref['source']['sequence'][f'rule_{"delex" if delex == "delex" else "lex"}'])))
 
         eval_order = mr_utils.remove_header(
             ref['source']['sequence'][eval_lin_strat])
@@ -126,23 +161,23 @@ def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
                 mr_utils.detokenize(beam_rr_toks), 
                 **ref['source']['mr']['slots']))
 
-        greedy_lmr = T2LMR(rules.tag_tokens(greedy_toks))
-        greedy_sem = prf(canon_ref_lmr, greedy_lmr)
-        greedy_ord = ord_prf(eval_order, greedy_lmr)
-        
-        beam_lmr = T2LMR(rules.tag_tokens(beam_toks))
-        beam_sem = prf(canon_ref_lmr, beam_lmr)
-        beam_ord = ord_prf(eval_order, beam_lmr)
+#        greedy_lmr = T2LMR(rules.tag_tokens(greedy_toks))
+#        greedy_sem = prf(canon_ref_lmr, greedy_lmr)
+#        greedy_ord = ord_prf(eval_order, greedy_lmr)
+#        beam_lmr = T2LMR(rules.tag_tokens(beam_toks))
+#        beam_sem = prf(canon_ref_lmr, beam_lmr)
+#        beam_ord = ord_prf(eval_order, beam_lmr)
 
         beam_rr_lmr = T2LMR(rules.tag_tokens(beam_rr_toks))
+        beam_rr_sem_err = sem_err(canon_ref_lmr, beam_rr_lmr)
         beam_rr_sem = prf(canon_ref_lmr, beam_rr_lmr)
         beam_rr_ord = ord_prf(eval_order, beam_rr_lmr)
 
-        all_results['greedy']['semantics'].append(greedy_sem)
-        all_results['greedy']['order'].append(greedy_ord)
-        all_results['beam']['semantics'].append(beam_sem)
-        all_results['beam']['order'].append(beam_ord)
-        all_results['beam_rr']['semantics'].append(beam_rr_sem)
+        #all_results['greedy']['semantics'].append(greedy_sem)
+        #all_results['greedy']['order'].append(greedy_ord)
+        #all_results['beam']['semantics'].append(beam_sem)
+        #all_results['beam']['order'].append(beam_ord)
+        all_results['beam_rr']['semantics'].append(beam_rr_sem_err['counts'])
         all_results['beam_rr']['order'].append(beam_rr_ord)
 
         if log_file != None:
@@ -162,35 +197,38 @@ def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
                                 subsequent_indent='    '),
                   end='\n\n', file=log_file)
 
-            print((
-                'Greedy:         (SEM P={:0.3f} R={:0.3f} F={:0.3f})'
-                '  (ORD P={:0.3f} R={:0.3f} F={:0.3f})\n'
-                ).format(*greedy_sem, *greedy_ord), file=log_file)
-            print(textwrap.fill(pretty_outputs["greedy"][-1], 
-                                initial_indent='    ', 
-                                subsequent_indent='    '),
-                  end="\n\n", file=log_file)
-
-            print((
-                'Beam:           (SEM P={:0.3f} R={:0.3f} F={:0.3f})'
-                '  (ORD P={:0.3f} R={:0.3f} F={:0.3f})\n'
-                ).format(*beam_sem, *beam_ord), file=log_file)
-
-            print(textwrap.fill(pretty_outputs["beam"][-1], 
-                                initial_indent='    ', 
-                                subsequent_indent='    '),
-                  end="\n\n", file=log_file)
+#?            print((
+#?                'Greedy:         (SEM P={:0.3f} R={:0.3f} F={:0.3f})'
+#?                '  (ORD P={:0.3f} R={:0.3f} F={:0.3f})\n'
+#?                ).format(*greedy_sem, *greedy_ord), file=log_file)
+#?            print(textwrap.fill(pretty_outputs["greedy"][-1], 
+#?                                initial_indent='    ', 
+#?                                subsequent_indent='    '),
+#?                  end="\n\n", file=log_file)
+#?
+#?            print((
+#?                'Beam:           (SEM P={:0.3f} R={:0.3f} F={:0.3f})'
+#?                '  (ORD P={:0.3f} R={:0.3f} F={:0.3f})\n'
+#?                ).format(*beam_sem, *beam_ord), file=log_file)
+#?
+#?            print(textwrap.fill(pretty_outputs["beam"][-1], 
+#?                                initial_indent='    ', 
+#?                                subsequent_indent='    '),
+#?                  end="\n\n", file=log_file)
             print((
                 'Reranked Beam:  (SEM P={:0.3f} R={:0.3f} F={:0.3f})'
                 '  (ORD P={:0.3f} R={:0.3f} F={:0.3f})\n'
                 ).format(*beam_rr_sem, *beam_rr_ord), file=log_file)
-
-
+            print("              M: {}  I: {}  A: {}\n".format(*beam_rr_sem_err['counts'][:3]),
+                file=log_file)
             print(textwrap.fill(pretty_outputs["beam_rr"][-1], 
                                 initial_indent='    ', 
                                 subsequent_indent='    '),
                   end="\n\n", file=log_file)
 
+            print("Missing   : {}".format(', '.join(beam_rr_sem_err['missing'])), file=log_file)
+            print("Incorrect : {}".format(', '.join(beam_rr_sem_err['incorrect'])), file=log_file)
+            print("Added     : {}".format(', '.join(beam_rr_sem_err['added'])), file=log_file)
 
             print("\n\n", file=log_file, flush=True)
 
@@ -211,9 +249,12 @@ def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
 #            assert ref['source']['mr'] == pred['mr']
 #
 #   
-    for sch in ['greedy', 'beam', 'beam_rr']:
-        all_results[sch]['semantics'] = np.array(
-            all_results[sch]['semantics']).mean(0)
+    for sch in ['beam_rr']:
+        se = np.array(all_results[sch]['semantics'])
+        print(se)
+        se = 1.0 * np.array(all_results[sch]['semantics']).sum(0)
+        se[3] = se[3] / len(all_results[sch]['semantics'])
+        all_results[sch]['semantics'] = se
         all_results[sch]['order'] = np.array(
             all_results[sch]['order']).mean(0)
 
@@ -221,20 +262,23 @@ def evaluate(refs, in_fp, rule_set, eval_lin_strat, name, eval_log_dir):
         for ref in refs:
             print(ref['target']['reference_strings'], end='\n\n', 
                   file=ref_fp, flush=True)
-        for sch in pretty_outputs.keys():
+        for sch in ['beam_rr']:
+        #for sch in pretty_outputs.keys():
             with NamedTemporaryFile("w") as pred_fp:
                 print("\n".join(pretty_outputs[sch]), file=pred_fp, flush=True)
                 r = compute_autoeval(
-                    Path("../../v2/eval/e2e-metrics/measure_scores.py"),
+                    str(script_path),
                     ref_fp.name, pred_fp.name)
                 all_results[sch]['auto'] = r
     return all_results
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('eval_script', type=Path)
     parser.add_argument('rule_set', choices=['Viggo', 'E2E'])
     parser.add_argument('ref', type=Path)
     parser.add_argument("pred", type=str, nargs='+')
+    parser.add_argument("--autometrics", nargs="+", default=["BLEU", "ROUGE_L", "NIST", "METEOR", "CIDEr"])
     parser.add_argument('--log-dir', type=Path, default=None)
 
     args = parser.parse_args()
@@ -245,24 +289,71 @@ def main():
     with args.ref.open('r') as fp:
         refs = [json.loads(line) for line in fp]
 
+    all_results = []        
     for arg in args.pred:
-        path, eval_lin_strat, name = arg.split(',')
-        
+        path, eval_lin_strat, name, model_type, lin_strat, = arg.split(',')
+
         with open(path, 'r') as fp:
 
-            results = evaluate(refs, fp, args.rule_set, eval_lin_strat, name,
-                               args.log_dir)
-
-            print(name)
-            for sch in ['greedy', 'beam', 'beam_rr']:
-                print("{:35s}  {:0.3f}  {:0.3f}  {:0.3f}  {:0.3f}".format(
+            results = evaluate(args.eval_script, refs, fp, args.rule_set, 
+                               eval_lin_strat, name, args.log_dir)
+            all_results.append({"model_type": model_type, "lin_strat": lin_strat, "results": results})
+            print("{:20}  {:10s}  {:4s}  {:4s}  {:4s}  {:4s}  {:4s}  {:4s}  {:4s}".format(
+                "model", "search", "BLEU", "METEOR", "NIST", "CIDEr", "ROUGE-L", "SEM", "ORD"))
+            for sch in ['beam_rr']: #['greedy', 'beam', 'beam_rr']:
+                print("{:20s}  {:10s}  {:0.3f}  {:0.3f}  {:0.3f}  {:0.3f}  {:0.3f}  {:0.3f}  {:0.3f}".format(
+                    name,
                     sch, 
                     results[sch]['auto']['BLEU'], 
+                    results[sch]['auto']['METEOR'], 
+                    results[sch]['auto']['NIST'], 
+                    results[sch]['auto']['CIDEr'], 
                     results[sch]['auto']['ROUGE_L'], 
-                    results[sch]['semantics'][2], 
+                    results[sch]['semantics'][3], 
                     results[sch]['order'][2]))
 
+    model_type_order = ['BiGru', 'Transformer']
+    lin_strat_order = ['Rand', 'IncFreq', 'IncFreqFixed', 'Align (F)', 'Align (M)', 'Align (O)']
 
+    all_results.sort(key=lambda x: lin_strat_order.index(x['lin_strat']))
+    all_results.sort(key=lambda x: model_type_order.index(x['model_type']))
+
+    
+    header = "    Model & Lin. Str. & " + " & ".join(args.autometrics) 
+    header += " & M & I & A & Err"
+    header += " & Ord."
+    header += r" \\"
+    header = header.replace("ROUGE_L", "RG-L")
+    header = header.replace("METEOR", "MET.")
+
+    cols = len(args.autometrics) + 4 + 1
+    
+    model_type_counts = Counter([r['model_type'] for r in all_results])
+    print(r"\begin{tabular}{ll" + "c" * cols + "}")
+    print(r"    \toprule")
+    print(r" & & \multicolumn{" +str(len(args.autometrics)) +r"}{c}{Automatic Metrics} & \multicolumn{4}{c}{Semantic} & \\")
+    print(header)
+    has_mtype = set()
+    for result in all_results:
+        if result['model_type'] in has_mtype:
+            mtype = ''
+        else:
+            print(r"\midrule")
+            mtype = (
+                r'\multirow{' 
+                + str(model_type_counts[result['model_type']]) 
+                + r"}{*}{\textsc{" + result['model_type'] + "}}"
+            )
+            has_mtype.add(result['model_type'])
+        print(mtype, "&", result['lin_strat'], "&", 
+            " & ".join([f'{result["results"]["beam_rr"]["auto"][m]:0.3f}' for m in args.autometrics]),
+            " & " + " & ".join([f'{int(x)}' for x in result['results']['beam_rr']['semantics'][:3]]),
+            f" & {result['results']['beam_rr']['semantics'][3] * 100:0.3f}",
+            " & {:0.3f}".format(result['results']['beam_rr']['order'][2]) if result['lin_strat'].startswith("Alig") else "& --",
+            r" \\")
+
+    print(r"    \bottomrule")
+    print(r"\end{tabular}")
 
 if __name__ == "__main__":
     main()
--- mrt/generate_text.py
+++ mrt/generate_text.py
@@ -1,88 +1,184 @@
 import torch
 import json
+from pathlib import Path
 
+import plum2
 from plum.types import Variable
 from plum2.tasks.task import Task
 from plum2.search import GreedySearch, BeamSearch
 
+import mrt.eval_utils as eval_utils
+import mrt.e2e.mr_utils
+import mrt.e2e.rules
+import mrt.viggo.mr_utils
+import mrt.viggo.rules
+
+import multiprocessing as mp
+
+
 
 class GenerateText(Task):
 
-    def __init__(self, model_path, dataset, encoder_vocab, decoder_vocab,
-                 order, output_prefix, search_widths=None):
+    def __init__(self, model_path, rule_set, dataset_path, lin_strat, 
+                 src_vocab, tgt_vocab, output_path,
+                 orig_mr='false', beam_size=8):
         super(GenerateText, self).__init__()
-
-        if search_widths == None:
-            search_widths = [1, 8]
         self.model_path = model_path
-        self.dataset = dataset
-        self.encoder_vocab = encoder_vocab
-        self.decoder_vocab = decoder_vocab
-        self.order = order
-        self.search_widths = search_widths
-        self.output_prefix = output_prefix
+        self.rule_set = rule_set
+        self.dataset_path = dataset_path
+        self.orig_mr = orig_mr
+        self.lin_strat = lin_strat
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
+        self.beam_size = beam_size
+        self.output_path = output_path
+
+    def input_iter(self):
+        with open(str(self.dataset_path), 'r') as fp:
+            for line in fp:
+                yield line
+                #ex = json.loads(line)
+                #yield ex
+
+    def get_reranked_index(self, results):
+        results = list(results)
+        
+        results.sort(key=lambda x: x['err']['f1'], reverse=True)
+        #results.sort(key=lambda x: x['mean_log_prob'], reverse=True)
+        
+        
+        for i, result in enumerate(results):
+            result['reranked_beam_candidate_num'] = i
+            #print(f"{result['err']['f1']:0.4f}  {result['mean_log_prob']} {result['beam_candidate_num']}")
+
+#        input()
+        return results[0]['reranked_beam_candidate_num']        
 
     def make_batch(self, input_tokens, gpu):
-        input_indices = [self.encoder_vocab.index(t) for t in input_tokens]
+        input_indices = [self.src_vocab.index(t) for t in input_tokens]
+        input_indices = [self.src_vocab.start_index] \
+            + input_indices + [self.src_vocab.stop_index]
         input_indices = torch.LongTensor([input_indices])
-        lengths = torch.LongTensor([len(input_tokens)])
-
+        lengths = torch.LongTensor([len(input_tokens) + 2])
         variable = Variable(input_indices.t(), lengths=lengths,
                             length_dim=0, batch_dim=1,
-                            pad_value=self.encoder_vocab.pad_index).cuda(gpu)
-
-            
+                            pad_value=self.src_vocab.pad_index).cuda(gpu)
         return {'encoder_input': variable}
 
+
+   
+
+
     def run(self, env, verbose=False):
-        output_path = env['proj_dir'] / (
-            self.output_prefix + "." + str(self.order) + ".jsonl")
-
-        output_path.parent.mkdir(exist_ok=True, parents=True)
-
-        
-        model = torch.load(self.model_path, 
+        self.env = env
+        model = torch.load(str(self.model_path),
                            map_location=lambda storage, loc: storage)
         model.eval()
         if env['gpu'] > -1:
             model = model.cuda(env['gpu'])
 
-        searches = {}
+        self.model = model
 
-        for w in self.search_widths:
-            if w == 1:
-                searches['greedy'] = GreedySearch(self.decoder_vocab,
-                                                  max_steps=100)
-            else:
-                searches[f'beam{w}'] = BeamSearch(self.decoder_vocab,
-                                                  max_steps=100,
-                                                  beam_size=w)
+        output_path = Path(str(self.output_path))
+        output_path.parent.mkdir(parents=True, exist_ok=True)
 
+        pool = mp.Pool(8)
+        print(f"Writing: {output_path}")
         with output_path.open("w") as fp:
-            for i, ex in enumerate(self.dataset, 1):
-                print(f"{i}/{len(self.dataset)}", end='\r', flush=True)
-                input_tokens = (
-                    [self.encoder_vocab.start_token] 
-                    + ex['source']['sequence'][self.order]
-                    + [self.encoder_vocab.stop_token] 
-                )
-                batch = self.make_batch(input_tokens, env['gpu'])
-                encoder_state = model.encode(batch)
+            for ex_id, ex in enumerate(pool.imap(self.process_example, self.input_iter())):
+          #  for ex_id, line in enumerate(self.input_iter()):
+                print(f"{ex_id}\r", end='', flush=True)
 
-                result = {
-                    'mr': ex['source']['mr'],
-                    'encoder_input': input_tokens,
-                    'outputs': {},
-                }
+           #     ex = self.process_example(line)
 
-                for search_name, search in searches.items():
-                    if search_name == 'greedy':
-                        search(model.decoder, encoder_state)
-                        result['outputs']['greedy'] = search.output()[0]
-                    else:
-                        k = int(search_name[4:])
-                        search(model.decoder, encoder_state)
-                        result['outputs'][search_name] = search.output(
-                            n_best=k)[0]
-                print(json.dumps(result), file=fp)
-            print()
+                print(json.dumps(ex), file=fp, flush=True)
+                    
+
+    def process_example(self, line):
+        if self.rule_set == 'E2E':
+            rules = mrt.e2e.rules
+            mr_utils = mrt.e2e.mr_utils
+        elif self.rule_set == 'Viggo':
+            rules = mrt.viggo.rules
+            mr_utils = mrt.viggo.mr_utils
+        else:
+            raise Exception(f"Bad rule set: {self.rule_set}")
+
+
+        model = self.model
+        sch = BeamSearch(self.tgt_vocab, max_steps=100, 
+                         beam_size=self.beam_size)
+
+        use_orig = str(self.orig_mr) == 'true'
+
+
+        ex = json.loads(line)
+        if use_orig:
+            mr = ex['orig']['mr']
+        else:
+            mr = ex['source']['mr']
+
+        if str(self.lin_strat).startswith("random"):
+            all_input_tokens = []
+            for i in range(5):
+                delex = str(self.lin_strat).endswith("delex")
+                input_tokens = mr_utils.linearize_mr(
+                    mr, delex=delex, order='random')
+                all_input_tokens.append(input_tokens)
+        else:
+            all_input_tokens = [
+                ex['source']['sequence'][str(self.lin_strat)]]
+
+        results = []
+        for input_tokens in all_input_tokens:
+
+            batch = self.make_batch(input_tokens, self.env['gpu'])
+            input_tokens_no_header = mr_utils.remove_header(
+                input_tokens)
+            input_tokens_no_header = [
+                t for t in input_tokens_no_header
+                if not t.endswith("N/A")
+            ]
+            
+            encoder_state = model.encode(batch)
+            sch(model.decoder, encoder_state)
+
+            scores = sch._beam_scores[0].tolist()
+            for i, output in enumerate(
+                    sch.output(n_best=self.beam_size)[0]):
+                pretty_output = mr_utils.detokenize(output)
+                if str(self.lin_strat).endswith('delex'):
+                    pretty_output = mr_utils.lexicalize_string(
+                        pretty_output, **mr["slots"])
+    
+
+                mr_seq = rules.tag_tokens(output, **mr['slots'])
+                lmr = mr_utils.tags2linear_mr(mr_seq, flip=True)
+                prec, recall, fscore = eval_utils.lmr_prf(
+                    input_tokens_no_header, lmr)
+                results.append(
+                    {
+                        "beam_candidate_num": i,
+                        "pretty": pretty_output, 
+                        "tokens": output,
+                        "tags": mr_seq,
+                        "pred_lmr": lmr,
+                        "mean_log_prob": scores[i],
+                        "err": {
+                            'precision': prec, 
+                            'recall': recall, 
+                            'f1': fscore},
+                    })
+        best_beam_cand = self.get_reranked_index(results)
+        ex_out = {
+            "mr": mr,
+            "use_orig": use_orig,
+            "lin_strat": str(self.lin_strat),
+            "input": input_tokens,
+            "outputs": results,
+            "input_slot_fillers": input_tokens_no_header,
+            "reranked_beam_output_index": best_beam_cand,
+        }
+        if 'target' in ex:
+            ex_out['references'] = ex['target']['reference_strings']
+        return ex_out
