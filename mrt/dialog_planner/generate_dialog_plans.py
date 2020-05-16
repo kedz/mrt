fc1e8c0c03f8b9f986a819f2c0e0a915feb656e5
Chris Kedzie <kedzie@cs.columbia.edu>
2020-05-16 11:38:50 -0400

    Updated dialog model and generation task.

--- mrt/dialog_planner/generate_dialog_plans.py
+++ mrt/dialog_planner/generate_dialog_plans.py
@@ -1,22 +1,28 @@
+from plum.types import Variable
 from plum2.tasks.task import Task
 from plum2.tasks import TASKS
-from permmr.pointer_search import GreedySearch, BeamSearch
+from plum2.search import GreedySearch, BeamSearch
+from mrt.dialog_planner.constrained_beam_search import ConstrainedBeamSearch
 import torch
+import numpy as np
 import json
+from tempfile import NamedTemporaryFile
+import shutil
 
 
 
-def add_dialog_plans(task_name, model_path, dataset_path, pipeline, vocab):
-    TASKS[task_name] = GenerateDialogPlans(model_path, dataset_path, pipeline,
-                                           vocab)
+def add_dialog_plans(task_name, model_path, dataset_path, vocab, mr_utils, lin_strat):
+    TASKS[task_name] = GenerateDialogPlans(model_path, dataset_path, vocab,
+                                           mr_utils, lin_strat)
 
 class GenerateDialogPlans(Task):
-    def __init__(self, model_path, dataset_path, pipeline, vocab):
+    def __init__(self, model_path, dataset_path, vocab, mr_utils, lin_strat):
         super(GenerateDialogPlans, self).__init__()
         self.model_path = model_path
         self.dataset_path = dataset_path
-        self.pipeline = pipeline
         self.vocab = vocab
+        self.mr_utils = mr_utils
+        self.lin_strat = lin_strat
 
     def batch2gpu(self, batch, gpu):
         if isinstance(batch, dict):
@@ -31,48 +37,73 @@ class GenerateDialogPlans(Task):
             return batch.cuda(gpu)
         else:
             return batch
- 
+
+
+    def make_batch(self, seq, gpu):
+        tokens = [self.vocab.start_token] + seq + [self.vocab.stop_token]
+        idxs = torch.LongTensor([[self.vocab.index(t)] for t in tokens])
+        lengths = torch.LongTensor([len(tokens)])
+        encoder_inputs = Variable(
+            idxs, lengths, batch_dim=1, length_dim=0, pad_value=0)
+        return {'encoder_input': encoder_inputs.cuda(gpu)}
+
+    def process(self, example, model, gpu):
+        ref_lmr_wheader = example['source']['sequence'][f'{self.lin_strat}_delex']
+        #ref_lmr_wheader = self.mr_utils.linearize_mr(
+        #    example['source']['mr'], delex=True)
+        header = self.mr_utils.get_header(ref_lmr_wheader)
+        ref_lmr = self.mr_utils.remove_header(ref_lmr_wheader)
+
+#        print(example['source']['sequence']['inc_freq_fixed_delex']) 
+        batch = self.make_batch(
+            example['source']['sequence'][f'{self.lin_strat}_delex'],
+            gpu) 
+        encoder_state = model.encode(batch)
+        search = ConstrainedBeamSearch(self.vocab, beam_size=32, max_steps=20)
+
+        search(model.decoder, encoder_state)
+        output = search.output()[0][:-1]
+
+        if len(output) < len(ref_lmr):
+            print("BADNESS!")
+            output = output + [x for x in ref_lmr if x not in output]
+
+        example['source']['sequence']['nlm_delex'] = header + output
+        assert set(example['source']['sequence']['nlm_delex']) \
+            == set(example['source']['sequence'][f'{self.lin_strat}_delex'])
+
+        
+        slots = {x.split("=")[0]: x.split("=")[1] 
+                 for x in self.mr_utils.remove_header(
+                     example['source']['sequence'][f'{self.lin_strat}_lex'])}
+        output_lex = []
+        for t in output:
+            if t.endswith("PLACEHOLDER"):
+                slot = t.split("=")[0]
+                output_lex.append(f'{slot}={slots[slot]}')
+            elif "SPECIFIER" in t:
+                slot = t.split("=")[0]
+                output_lex.append(f'{slot}={slots[slot]}')
+            else:
+                output_lex.append(t)
+        example['source']['sequence']['nlm_lex'] = header + output_lex
+        assert set(example['source']['sequence']['nlm_lex']) \
+            == set(example['source']['sequence'][f'{self.lin_strat}_lex'])
 
     def run(self, env, verbose=False):
 
-        model = torch.load(self.model_path, 
+        model = torch.load(str(self.model_path), 
                            map_location=lambda storage, loc: storage).eval()
         if env['gpu'] > -1:
             model = model.cuda(env['gpu'])
 
-        search = BeamSearch(self.vocab, beam_size=2, max_steps=20)
-#        search = GreedySearch(self.vocab, max_steps=20)
-
-        with open(self.dataset_path, 'r') as in_fp:
-            batches = []
-            for line in in_fp:
-                example = json.loads(line)
-                batches.append(example)
-                if len(batches) == 4:
-                #batch = self.pipeline([example])
-                    batch = self.pipeline(batches)
-                    if env['gpu'] > -1:
-                        batch = self.batch2gpu(batch, env['gpu'])
-                    encoder_state = model.encode(batch)
-                    encoder_state['source_indices'] = batch['encoder_input']
-                    search(model.decoder, encoder_state)
-            
-                    plans = search.output() #(n_best=4)
-                    for b, pred_plan in enumerate(plans):
-                        pred_plan = pred_plan[:-1]
-                        #pr
-                        print(pred_plan)
-                        ref_plan = batches[b]['source']['sequence']['random'][2:]
-                        print(ref_plan)
-                        #print(plan[1][:-1])
-                        #print(plan[2][:-1])
-                        #print(plan[3][:-1])
-                        print()
-
-                    #print(plan)
-                        assert len(pred_plan) == len(ref_plan)
-                        assert set(ref_plan) == set(pred_plan)
-            #print(batches[1]['source']['sequence']['oracle'][2:])
-            #print(batches[2]['source']['sequence']['oracle'][2:])
-            #assert len(plan) == len(set(example['source']['sequence']['random'][2:]))
-                    batches = []
+        with NamedTemporaryFile('w') as out_fp:
+            with open(str(self.dataset_path), 'r') as in_fp:
+                for i, line in enumerate(in_fp, 1):
+                    print(f'processing: {i}', end='\r', flush=True)
+                    example = json.loads(line)
+                    self.process(example, model, env['gpu'])
+                    print(json.dumps(example), file=out_fp)
+                print()
+            out_fp.flush()
+            shutil.copy(out_fp.name, str(self.dataset_path))
--- mrt/dialog_planner/model.py
+++ mrt/dialog_planner/model.py
@@ -1,129 +1,246 @@
 from plum2.object2 import Builder, LazyObject, no_init
 from plum2.models.model import Model
 import plum2.layer as layer
-from plum2.models.seq2seq import EncoderDecoder
-from mrt.dialog_planner.passthru_predictor import PassThruPredictor
-from mrt.dialog_planner.pointer_decoder_wrapper import PointerDecoderWrapper
-from mrt.dialog_planner.pointer_search import GreedySearch, BeamSearch
+from plum2.search import GreedySearch, BeamSearch
+from mrt.dialog_planner.constrained_beam_search import ConstrainedBeamSearch
 from contextlib import contextmanager
-
 
 from plum2.models import MODELS
 
 @contextmanager
 def dialog_planner(name):
-    b = DialogPlannerBuilder()
+    b = Seq2SeqModelBuilder()
     yield b
     m = b.finish()
     MODELS[name] = m
     b.forward()
 
 
-class DialogPlannerBuilder(Builder):
+
+class EncoderDecoder(Model):
+
+    def __init__(self, encoder, decoder, encoder_inputs=None,
+                 decoder_input=None):
+        super(EncoderDecoder, self).__init__()
+        if encoder_inputs is None:
+            encoder_inputs = ["encoder_input"]
+        if decoder_input is None:
+            decoder_input = "decoder_input"
+        self.encoder = encoder
+        self.decoder = decoder
+        self.encoder_inputs = encoder_inputs
+        self.decoder_input = decoder_input
+        self.searches = {}
+
+    def __getstate__(self):
+        object.__getattribute__(self, "init_deps")()
+        state = {
+            key: (val if key not in ["_dependencies", "searches"] else {})
+            for key, val in object.__getattribute__(self, "__dict__").items()
+        }
+        return state
+
+    @no_init
+    def add_search(self, name, search):
+        self.searches[name] = search
+
+    def encode(self, batch):
+        encoder_args = [batch[input] for input in self.encoder_inputs]
+
+        state = self.encoder(*encoder_args)
+        state['cons'] = encoder_args[0]
+        return state
+
+    def forward(self, batch):
+        encoder_state = self.encode(batch)
+        decoder_state = self.decoder(batch[self.decoder_input], encoder_state)
+        decoder_state["encoder_state"] = encoder_state
+        if len(self.searches):
+            decoder_state["search"] = {
+                search: LazyObject(lambda: search_func(encoder_state))
+                for search, search_func in self.searches.items()
+            }
+            #print(list(decoder_state["search"].keys()))
+        return decoder_state
+
+
+class Seq2SeqModelBuilder(Builder):
 
     def __init__(self):
-        super(DialogPlannerBuilder, self).__init__()
-
+        super(Seq2SeqModelBuilder, self).__init__()
+        self._arch = None
         self._layers = 1
+        self._hidden_size = None
+        self._emb_size = None
+        self._enc_vocab = None
+        self._dec_vocab = None
+        self._bidir = False
+        self._attention = None
         self._dropout = 0.0
-        self._emb_feats = None
-        self._hidden_feats = None
-        self._vocab = None
+        self._bridge = None
         self._searches = None
-        self._tie_embs = False
+        self._tie_decoder_embeddings = False
+    
+    def arch(self, val):
+        if val not in ["rnn", "gru", "lstm", "transformer"]:
+            raise Exception("Invalid architecture.")
+        self._arch = val
+        return self
 
-    def tie_embs(self, val=True):
-        self._tie_embs = val
+    def tie_decoder_embeddings(self, val=True):
+        self._tie_decoder_embeddings = val
+        return self
+
+    def emb(self, dims):
+        self._emb_size = dims
+        return self
+ 
+    def hidden(self, dims):
+        self._hidden_size = dims
         return self
     
+    def enc_vocab(self, vocab):
+        self._enc_vocab = vocab
+        return self
+
+    def dec_vocab(self, vocab):
+        self._dec_vocab = vocab
+        return self
+    
+    def bi(self, val=True):
+        self._bidir = val
+        return self   
+
     def layers(self, val):
         self._layers = val
-        return self
-
-    def emb(self, val):
-        self._emb_feats = val
-        return self
-
-    def hidden(self, val):
-        self._hidden_feats = val
         return self
 
     def dropout(self, val):
         self._dropout = val
         return self
 
-    def vocab(self, val):
-        self._vocab = val
+    def attn(self, val):
+        if val not in layer.attention.TYPES or val == None:
+            raise ValueError(f"Invalid attention method: {method} "
+                             f"must be one of {layer.attention.TYPES}")
+        self._attention = val
         return self
 
-    def beam_search(self, beam_size=4, max_steps=20, name="beam"):
+    def beam_search(self, beam_size=8, max_steps=100, name="beam"):
         if self._searches is None:
             self._searches = {}
-        self._searches[name] = BeamSearch(self._vocab, 
-                                          beam_size=beam_size,
-                                          max_steps=max_steps)
+        self._searches[name] = ConstrainedBeamSearch(
+            self._dec_vocab, beam_size=beam_size, max_steps=max_steps)
         return self
 
+    def greedy_search(self, max_steps=100, name="greedy"):
+        if self._searches is None:
+            self._searches = {}
+        self._searches[name] = GreedySearch(self._dec_vocab, 
+                                            max_steps=max_steps)
+        return self
+    
+    def pass_thru_bridge(self):
+        self._bridge = layer.seq.PassThruBridge()
+        return self
 
 
     def _finish_object(self):
-        enc_emb = layer.Embedding(
-            self._vocab.size(),
-            self._emb_feats,
-            pad_index=self._vocab.pad_index,
-            dropout=self._dropout)
-        enc = layer.seq.TransformerEncoder(
-            enc_emb, hidden_feats=self._hidden_feats,
-            layers=self._layers, dropout=self._dropout)
-
-        dec_emb = layer.Embedding(
-                self._vocab.size(),
-                self._emb_feats,
-                pad_index=self._vocab.pad_index, 
-                dropout=self._dropout)
-
-        dec_pos_emb = layer.SinusoidalEmbedding(
-            5000, 
-            self._emb_feats,
-            dropout=self._dropout)
-        dec_input_module = layer.ParallelModule(
-            dec_emb, dec_pos_emb,
-            aggregate='sum')
-
-        predictor = PassThruPredictor()
-        #predictor = layer.LinearPredictor(
-        #    self._emb_feats, self._vocab.size())
-         
-        dec = PointerDecoderWrapper(
-            layer.seq.TransformerDecoder(
-                dec_input_module, self._hidden_feats, predictor,
-                layers=self._layers, dropout=self._dropout))
-
-        model = PointerEncoderDecoder(enc, dec)
-        if self._searches:
-            def make_search(model, s):
-                def f(encoder_state, controls=None):
-                    s(model.decoder, encoder_state, controls=controls)
-                    return s
-                return f
-
-            for name, search in self._searches.items():
-                model.add_search(name, make_search(model, search))
-
-        if self._tie_embs:
-            enc_emb.kernel = dec_emb.kernel
         
+        if self._arch == 'transformer':
+            
+            enc_emb = layer.Embedding(
+                self._enc_vocab.size(), self._emb_size, 
+                pad_index=self._enc_vocab.pad_index, dropout=self._dropout)
+            enc_pos_emb = layer.SinusoidalEmbedding(5000, self._emb_size,
+                                                    dropout=self._dropout)
+            enc_input_module = layer.ParallelModule(enc_emb, enc_pos_emb,
+                                                    aggregate='sum')
+            
+            enc = layer.seq.TransformerEncoder(
+                enc_input_module, hidden_feats=self._hidden_size, 
+                layers=self._layers, dropout=self._dropout)
+
+            dec_emb = layer.Embedding(
+                self._dec_vocab.size(), self._emb_size,
+                pad_index=self._dec_vocab.pad_index, dropout=self._dropout)
+
+            dec_pos_emb = layer.SinusoidalEmbedding(5000, self._emb_size,
+                                                    dropout=self._dropout)
+            dec_input_module = layer.ParallelModule(dec_emb, dec_pos_emb,
+                                                    aggregate='sum')
+
+            predictor = layer.LinearPredictor(
+                self._emb_size, self._dec_vocab.size())
+ 
+            dec = layer.seq.TransformerDecoder(
+                dec_input_module, self._hidden_size, predictor,
+                layers=self._layers, dropout=self._dropout)
+
+            model = EncoderDecoder(enc, dec)
+
+            if self._searches:
+                def make_search(model, s):
+                    def f(encoder_state, controls=None):
+                        s(model.decoder, encoder_state, controls=controls)
+                        return s
+                    return f
+
+                for name, search in self._searches.items():
+                    model.add_search(name, make_search(model, search))
+
+        else:
+            enc_emb = layer.Embedding(
+                self._enc_vocab.size(), self._emb_size,
+                pad_index=self._enc_vocab.pad_index, dropout=self._dropout)
+
+            #if self._bidir and 
+            if self._bridge == None:
+                self._bridge = layer.seq.FeedForwardBridge(
+                    self._hidden_size, self._bidir, self._arch, self._layers,
+                    dropout=self._dropout)
+
+            enc = layer.seq.RNNEncoder(enc_emb, self._hidden_size, self._arch,
+                                       bidirectional=self._bidir,
+                                       layers=self._layers, dropout=self._dropout,
+                                       bridge=self._bridge)
+                                       
+            enc_out_feats = enc.out_feats
+            dec_emb = layer.Embedding(
+                self._dec_vocab.size(), self._emb_size,
+                pad_index=self._dec_vocab.pad_index, dropout=self._dropout)
+
+            if self._attention != None:
+                attn = layer.attention.attention(enc_out_feats, self._hidden_size,
+                                                 method=self._attention)
+            else:
+                attn = None
+
+            adapter_feats = self._hidden_size + (attn != None) * enc.out_feats  
+
+            predictor = layer.LinearPredictor(
+                self._hidden_size, self._dec_vocab.size(),
+                adapter_feats=adapter_feats, adapter_activation="tanh")
+
+            dec = layer.seq.RNNDecoder(dec_emb, self._hidden_size, predictor,
+                                       self._arch, layers=self._layers,
+                                       dropout=self._dropout,
+                                       attention_module=attn)
+           
+            model = EncoderDecoder(enc, dec)
+
+            if self._searches:
+                def make_search(model, s):
+                    def f(encoder_state, controls=None):
+                        s(model.decoder, encoder_state, controls=controls)
+                        return s
+                    return f
+
+                for name, search in self._searches.items():
+                    model.add_search(name, make_search(model, search))
+
+        if self._tie_decoder_embeddings:
+            Y = dec_emb.kernel 
+            predictor._tie_embeddings = dec_emb
 
         return model
-
-
-class PointerEncoderDecoder(EncoderDecoder):
-    def __init__(self, *args, source_indices='encoder_input', **kwargs):
-        super(PointerEncoderDecoder, self).__init__(*args, **kwargs)
-        self.source_indices = source_indices
-
-    def encode(self, batch):
-        encoder_state = super(PointerEncoderDecoder, self).encode(batch)
-        encoder_state['source_indices'] = batch[self.source_indices]
-        return encoder_state
-
